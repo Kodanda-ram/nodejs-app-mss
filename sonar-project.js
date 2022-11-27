91db42438db92981ac13ed5031207a94b7c8789d
Kodanda-ram <91193891+Kodanda-ram@users.noreply.github.com>
2022-11-27 22:05:09 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.205.111.47:9000/',
+  serverUrl: 'http://43.205.135.109:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
